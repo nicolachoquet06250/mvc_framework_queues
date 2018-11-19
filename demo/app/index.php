b5810571c882d72e1843069618356e91337837b5
nicolas.choquet <nicolas.choquet@lagardere-active.com>
2018-11-19 09:10:30 +0100

    envoie d'un vrai email.

---
+++ demo/app/index.php
@@ -0,0 +1,28 @@
+<?php
+
+use mvc_framework\core\queues\traits\Queue;
+
+require_once __DIR__.'/../autoload.php';
+
+header('Content-Type: application/json');
+
+$queue_email_sender = \mvc_framework\core\queues\traits\Queue::instance('email', Queue::$SEND);
+$email = [
+	'to' => $_POST['to'],
+	'from' => $_POST['from'],
+	'content' => $_POST['content'],
+	'object' => $_POST['object'],
+];
+$nb_envois = $_POST['nb_envois'];
+
+$cmp = 0;
+
+while ($cmp < $nb_envois) {
+	$queue_email_sender->send($email);
+}
+
+echo json_encode(
+	[
+		'success' => true,
+	]
+);
--- demo/classes/elements/EmailElement.php
+++ demo/classes/elements/EmailElement.php
@@ -7,9 +7,22 @@ use mvc_framework\core\queues\traits\QueueElement;
 
 class EmailElement {
 	use QueueElement;
+	private $to, $from, $content, $object;
+	public function __construct($element) {
+		parent::__construct($element);
+		$this->to = $this->get('to');
+		$this->from = $this->get('from');
+		$this->content = $this->get('content');
+		$this->object = $this->get('object');
+	}
+
 	public function execute() {
-		var_dump($this->get('email'));
-		var_dump($this->get('objet'));
-		var_dump($this->get('content'));
+		$headers = [
+			'From' => $this->from,
+			'Reply-To' => $this->from,
+			'X-Mailer' => 'PHP/' . phpversion()
+		];
+
+		mail($this->to, $this->object, str_replace("\n", '<br>', $this->content), $headers);
 	}
 }
